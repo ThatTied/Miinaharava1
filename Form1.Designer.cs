8fa30208332e23621b428113376fbb3eab73196a
Tied <51081414+ThatTied@users.noreply.github.com>
2020-09-03 12:47:59 +0300

    miinaharava

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsMiinaharava
+{
+    partial class Miinaharava
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Miinaharava));
+            this.helpponappi = new System.Windows.Forms.Button();
+            this.keskivaikeanappi = new System.Windows.Forms.Button();
+            this.vaikeanappi = new System.Windows.Forms.Button();
+            this.vaikeustasotext = new System.Windows.Forms.Label();
+            this.miinaTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // helpponappi
+            // 
+            this.helpponappi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.helpponappi.Location = new System.Drawing.Point(116, 57);
+            this.helpponappi.Name = "helpponappi";
+            this.helpponappi.Size = new System.Drawing.Size(75, 23);
+            this.helpponappi.TabIndex = 0;
+            this.helpponappi.Text = "9x9 Ruudut";
+            this.helpponappi.UseVisualStyleBackColor = false;
+            this.helpponappi.Click += new System.EventHandler(this.Helpponappi_Click);
+            // 
+            // keskivaikeanappi
+            // 
+            this.keskivaikeanappi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.keskivaikeanappi.Location = new System.Drawing.Point(116, 86);
+            this.keskivaikeanappi.Name = "keskivaikeanappi";
+            this.keskivaikeanappi.Size = new System.Drawing.Size(75, 23);
+            this.keskivaikeanappi.TabIndex = 1;
+            this.keskivaikeanappi.Text = "16x16";
+            this.keskivaikeanappi.UseVisualStyleBackColor = false;
+            this.keskivaikeanappi.Click += new System.EventHandler(this.Keskivaikeanappi_Click);
+            // 
+            // vaikeanappi
+            // 
+            this.vaikeanappi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.vaikeanappi.Location = new System.Drawing.Point(116, 116);
+            this.vaikeanappi.Name = "vaikeanappi";
+            this.vaikeanappi.Size = new System.Drawing.Size(75, 23);
+            this.vaikeanappi.TabIndex = 2;
+            this.vaikeanappi.Text = "30x16";
+            this.vaikeanappi.UseVisualStyleBackColor = false;
+            this.vaikeanappi.Click += new System.EventHandler(this.Vaikeanappi_Click);
+            // 
+            // vaikeustasotext
+            // 
+            this.vaikeustasotext.AutoSize = true;
+            this.vaikeustasotext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vaikeustasotext.Location = new System.Drawing.Point(92, 23);
+            this.vaikeustasotext.Name = "vaikeustasotext";
+            this.vaikeustasotext.Size = new System.Drawing.Size(111, 20);
+            this.vaikeustasotext.TabIndex = 3;
+            this.vaikeustasotext.Text = "Vaikeustaso";
+            // 
+            // miinaTextBox
+            // 
+            this.miinaTextBox.Location = new System.Drawing.Point(10, 10);
+            this.miinaTextBox.Name = "miinaTextBox";
+            this.miinaTextBox.ReadOnly = true;
+            this.miinaTextBox.Size = new System.Drawing.Size(40, 22);
+            this.miinaTextBox.TabIndex = 4;
+            this.miinaTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Miinaharava
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(323, 173);
+            this.Controls.Add(this.miinaTextBox);
+            this.Controls.Add(this.vaikeustasotext);
+            this.Controls.Add(this.vaikeanappi);
+            this.Controls.Add(this.keskivaikeanappi);
+            this.Controls.Add(this.helpponappi);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Miinaharava";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Miinaharava";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helpponappi;
+        private System.Windows.Forms.Button keskivaikeanappi;
+        private System.Windows.Forms.Button vaikeanappi;
+        private System.Windows.Forms.Label vaikeustasotext;
+        private System.Windows.Forms.TextBox miinaTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsMiinaharava
+{
+    public partial class Miinaharava : Form
+    {
+        public Miinaharava()
+        {
+            InitializeComponent();
+            miinaTextBox.Hide();
+        }
+        public List<int> Tile;
+        Button ruutu;
+        public Random Rnd = new Random();
+        public int Miinamaara = 0; // Miinojen määrä
+        public int RndMiinamaara; //Miinojen määrä miinojen istutusta varten
+        public int MaxMiinat; // Max miinat per kenttä
+        public int RuutuKoko = 26; // Ruutujen säädeltävä koko
+        public Image Miina = WindowsFormsMiinaharava.Properties.Resources.pienimiina;
+        public Image Lipputanko = WindowsFormsMiinaharava.Properties.Resources.lippu;
+        public bool IstutaMiina(ref Random Rand)
+        {
+            int chance = Rand.Next(RndMiinamaara);
+            if (chance < MaxMiinat && MaxMiinat > Miinamaara) // 10/40/99 > 81/256/480, jos on tarpeeksi miinoja, return false aina
+            {
+                Miinamaara++;
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+        public void FormKoko(int x, int y) // Säädeltävä Formin koko X ja Y -arvoilla
+        {
+            int KokoX;
+            int KokoY;
+            KokoX = 5 + (x * RuutuKoko) + (x * 2) + 20;
+            KokoY = 35 + (y * RuutuKoko) + (y * 2) + 40;
+            this.Size = new Size(KokoX, KokoY);
+        }
+        public void LuoKentta(int x, int y) // Luodaan miinakenttä, ruutujen määrää pystyy säätää X ja Y -arvoilla
+        {
+            Point newLoc = new Point(5, 35);
+            this.helpponappi.Hide(); // Piilotetaan aloitusruudun asiat
+            this.vaikeanappi.Hide();
+            this.keskivaikeanappi.Hide();
+            this.vaikeustasotext.Hide();
+
+            for (int o = 0; o < x; ++o) // X-akseli
+            {
+                for (int i = 0; i < y; ++i) // Y-akseli, luodaan ruudut
+                {
+                    ruutu = new Button(); // Luodaan uusi ruutu, ja asetetaan parametrit
+                    ruutu.Size = new Size(RuutuKoko, RuutuKoko);
+                    ruutu.Location = newLoc;
+                    newLoc.Offset(0, RuutuKoko + 2);
+                    ruutu.MouseUp += (object sender, MouseEventArgs e) => { // Lisätään ruudulle MouseUp eventti jotta voidaan katsoa vasen tai oikean hiirinklikkauksia
+                        Button tamaRuutu = sender as Button;
+                        bool OnPommi = IstutaMiina(ref Rnd);
+
+
+                        if (tamaRuutu.Image == Miina || tamaRuutu.BackColor == System.Drawing.Color.LightGray)
+                        {
+                            return;
+                        }
+                        else if (e.Button == System.Windows.Forms.MouseButtons.Left && tamaRuutu.Image == Lipputanko)
+                        {
+                            tamaRuutu.Image = null;
+                        }
+                        else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                        {
+                            tamaRuutu.Image = Lipputanko;
+                        }
+                        else if (e.Button == System.Windows.Forms.MouseButtons.Left && OnPommi == true)
+                        {
+                            tamaRuutu.Image = Miina;
+                            tamaRuutu.BackColor = System.Drawing.Color.Red;
+                        }
+                        else
+                        {
+                            tamaRuutu.BackColor = System.Drawing.Color.LightGray;
+                        }
+                    };
+                    Controls.Add(ruutu);
+                }
+                newLoc.Y = 35;
+                newLoc.Offset(RuutuKoko + 2, 0);
+            }
+            miinaTextBox.Show();
+            miinaTextBox.Text = Convert.ToString(Miinamaara);
+        }
+        private void Helpponappi_Click(object sender, EventArgs e)
+        {
+            MaxMiinat = 10;
+            RndMiinamaara = 81;
+            LuoKentta(9, 9);
+            FormKoko(9, 9);
+            miinaTextBox.Text = "10";
+        }
+
+        private void Keskivaikeanappi_Click(object sender, EventArgs e)
+        {
+            MaxMiinat = 40;
+            RndMiinamaara = 256;
+            LuoKentta(16, 16);
+            FormKoko(16, 16);
+            miinaTextBox.Text = "40";
+        }
+
+        private void Vaikeanappi_Click(object sender, EventArgs e)
+        {
+            MaxMiinat = 99;
+            RndMiinamaara = 480;
+            LuoKentta(30, 16);
+            FormKoko(30, 16);
+            miinaTextBox.Text = "99";
+        }
+    }
+}
