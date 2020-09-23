96176bb7da4b12bfce299e4e17dfac5d65a33f89
Tied <51081414+ThatTied@users.noreply.github.com>
2020-09-23 12:49:16 +0300

    Tile.cs erikseen + Draw.io diagrammi

--- Form1.cs
+++ Form1.cs
@@ -196,22 +196,5 @@ namespace WindowsFormsMiinaharava
             FormKoko(30, 16);
             miinaTextBox.Text = "99";
         }
-        public class Tile : Button
-        {
-            public int ID { get; set; }
-            public int X { get; set; }
-            public int Y { get; set; }
-            public bool IsMine { get; set; }
-            public int NaapuriPommit { get; set; }
-            public bool Painettu { get; set; }
-            public bool Liputettu { get; set; }
-
-            public Tile(int id, int x, int y)
-            {
-                ID = id;
-                X = x;
-                Y = y;
-            }
-        }
     }
 }
---
+++ Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class Tile : Button
+{
+    public int ID { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsMine { get; set; }
+    public int NaapuriPommit { get; set; }
+    public bool Painettu { get; set; }
+    public bool Liputettu { get; set; }
+
+    public Tile(int id, int x, int y)
+    {
+        ID = id;
+        X = x;
+        Y = y;
+    }
+}
